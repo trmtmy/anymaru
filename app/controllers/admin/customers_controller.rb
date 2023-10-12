c0a2664558d746ffd5aa6f981e9d4354f1e03b1f
EC2 Default User <ec2-user@ip-172-31-19-26.ap-northeast-1.compute.internal>
2023-10-12 06:44:20 +0000

    管理退会実装

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -14,7 +14,7 @@ class Admin::CustomersController < ApplicationController
   def update
     @customer = Customer.find(params[:id])
     if @customer.update(customer_params)
-     redirect_to admin_customer_path(@customer.id)
+     redirect_to admin_customers_path
     else
      @customers = Customer.all
      render :edit
