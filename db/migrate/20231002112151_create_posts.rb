5299210d8586aa32f873c5d9adccbd43767dbd34
EC2 Default User <ec2-user@ip-172-31-19-26.ap-northeast-1.compute.internal>
2023-10-17 11:46:06 +0000

    deploy マイグレーションエラーに対応

--- db/migrate/20231002112151_create_posts.rb
+++ db/migrate/20231002112151_create_posts.rb
@@ -1,8 +1,8 @@
 class CreatePosts < ActiveRecord::Migration[6.1]
   def change
     create_table :posts do |t|
-      t.text :introduction, null: false, default: ""
-      t.integer :category, null: false, default: ""
+      t.text :introduction, null: false
+      t.integer :category, null: false
       t.references :customer, foreign_key: true
       t.timestamps
     end
